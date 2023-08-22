e46c929504a684f031c95530b5c5d54d1890b410
aldodagio <103005119+aldodagio@users.noreply.github.com>
2023-08-22 12:49:11 -0700

    CaloriesBurned and CaloriesBurnedTest created to find calories burned for user based on activity. Two tests instantiated in CaloriesBurnedTest class for metric system input and english system input (dealing with heights and weights).

--- app/src/main/java/com/gymhomie/location/LocationHandler.java
+++ app/src/main/java/com/gymhomie/location/LocationHandler.java
@@ -31,7 +31,6 @@ public class LocationHandler {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String ipAddress = reader.readLine();
-
         reader.close();
         connection.disconnect();
 
