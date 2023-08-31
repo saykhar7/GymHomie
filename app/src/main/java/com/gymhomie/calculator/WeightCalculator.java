bb435aaa24ee76327a4946a514e13115858a3c7b
Gustavo Pech <31997991+gustavopech@users.noreply.github.com>
2023-08-31 16:11:47 -0700

    allowed the tools fragment to switch to barbell calculator fragment however the buttons did not switch.

---
+++ app/src/main/java/com/gymhomie/calculator/WeightCalculator.java
@@ -0,0 +1,34 @@
+/**
+ *     The WeightCalculator class will allow users
+ *     to input the desired weight(lbs) and then receive a list of
+ *     WeightedPlates needed in order to reach that total weight
+ *
+ */
+
+package com.gymhomie.calculator;
+
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class WeightCalculator {
+    ArrayList<WeightedPlate> platesAvailable = new ArrayList<>(); //final list
+
+
+
+    /**
+     *     The calculate RequiredPlates will take in a double
+     *     and output a list of required plates.
+     *
+     *     This is going to be done by using mod to the biggest WeightedPlate value
+     *
+     */
+    public ArrayList<WeightedPlate> calculateRequiredPlates( double desiredWeight){
+        ArrayList<WeightedPlate> requiredPlates = new ArrayList<>();
+
+
+        return requiredPlates;
+    }
+
+
+}
--- app/src/main/java/com/gymhomie/fragments/tools_fragment.java
+++ app/src/main/java/com/gymhomie/fragments/tools_fragment.java
@@ -2,22 +2,39 @@ package com.gymhomie.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.gymhomie.R;
 
 
 public class tools_fragment extends Fragment {
+    private Button barbell_Calculator;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tools, container, false);
+        View view = inflater.inflate(R.layout.fragment_tools, container, false);
+        barbell_Calculator = (view.findViewById(R.id.calculator_button));
+
+        barbell_Calculator.setOnClickListener(new View.OnClickListener(){
+            @Override
+           public void onClick(View view){
+            Fragment barcalc_frag = new barbellcalc_fragment();
+            FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+            fm.replace(R.id.frameLayout, barcalc_frag).commit();
+            }
+        });
+        return view;
     }
+
 }
