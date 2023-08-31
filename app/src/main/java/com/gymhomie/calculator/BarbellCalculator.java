3b061fcb101d2697a9d8e774f7eafbd90f831f00
Gustavo Pech <31997991+gustavopech@users.noreply.github.com>
2023-08-31 11:57:22 -0700

    Backend with comments

--- app/src/main/java/com/gymhomie/calculator/BarbellCalculator.java
+++ app/src/main/java/com/gymhomie/calculator/BarbellCalculator.java
@@ -1,28 +1,41 @@
+/**
+ *     The BarbellCalculator class will allow users
+ *     to view how much weight(LBs) is on the barbell(bar)
+ *     by inputting what weights are one side through the GUI buttons
+ *
+ */
+
 package com.gymhomie.calculator;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-//This will allow to see how much weight is on a barbell based on the plates loaded
+
 public class BarbellCalculator {
+    ArrayList<WeightedPlate> loadedPlates = new ArrayList<>(); //Keep a list of the plates WeightedPlates on the bar
+
+    /**
+     *     The calculateLoadedWeight method will take in a list
+     *     and iterate through that list and calculate the total weight
+     */
+    public double calculateLoadedWeight(){
+        double currentSum = 0; //default weight
+        double barbellWeight = 45; //Most barbells are 20 kilos so about 44 lbs but 45 is easier to work with
+        currentSum += barbellWeight; // Adding the barbell weight to current sum
+
+
+        for (int i = 0; i < loadedPlates.size() && i < 50; i++){ //50 is to cap total possible weight
+            currentSum += loadedPlates.get(i).getValue();
+        }
+        return currentSum;
+    }
+
+    /**
+     *     Setter and Getter for loadedPlates
+     */
     public ArrayList<WeightedPlate> getLoadedPlates() {
         return loadedPlates;
     }
-
     public void setLoadedPlates(ArrayList<WeightedPlate> loadedPlates) {
         this.loadedPlates = loadedPlates;
     }
-
-    ArrayList<WeightedPlate> loadedPlates = new ArrayList<WeightedPlate>();
-    double totalWeight;
-
-    public double getTotalWeight() {
-        return totalWeight;
-    }
-
-    public void setTotalWeight(double totalWeight) {
-        this.totalWeight = totalWeight;
-    }
-
-
-
-
 }
--- app/src/main/java/com/gymhomie/calculator/WeightedPlate.java
+++ app/src/main/java/com/gymhomie/calculator/WeightedPlate.java
@@ -2,8 +2,36 @@ package com.gymhomie.calculator;
 
 public class WeightedPlate {
 
+    //how much does the plate work
+    double value;
+
+
+
+    //how many plates
+    int quantity ;
+
     //The weight of the plates are measures in kilo's to simplify conversion
     public WeightedPlate(double value) {
-
+        this.value = value;
+        this.quantity = 0;
     }
+    public WeightedPlate(double value, int quantity) {
+        this.value = value;
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
 }
