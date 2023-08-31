9db4abce8009571b91e7e6b1d4c01ed203d08d36
Gustavo Pech <31997991+gustavopech@users.noreply.github.com>
2023-08-31 13:17:15 -0700

    Backend with comments

--- app/src/main/java/com/gymhomie/fragments/barbellcalc_fragment.java
+++ app/src/main/java/com/gymhomie/fragments/barbellcalc_fragment.java
@@ -1,37 +1,32 @@
 package com.gymhomie.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.gymhomie.NameViewModel;
 import com.gymhomie.R;
 
 
-public class tools_fragment extends Fragment {
+public class barbellcalc_fragment extends Fragment {
     private Button barbell_Calculator;
-    private profile_fragment.OnLogoutClickListener onLogoutClickListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tools, container, false);
         barbell_Calculator = (view.findViewById(R.id.barcalcBtn));
-        if (barbell_Calculator != null) {
-            barbell_Calculator.setOnClickListener((View.OnClickListener)(new View.OnClickListener() {
-                public final void onClick(View it) {
-                    setContentView(R.layout.activity_login);
-                }
-            }));
+
 
         return inflater.inflate(R.layout.fragment_tools, container, false);
-
     }
-
-
-
-
 }
