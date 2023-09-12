04bd090d6d263f243c497b762bb8fcaae5f6e40b
cienacamille <61853334+cienacamille@users.noreply.github.com>
2023-09-11 23:48:21 -0700

    edited workout and exercise class edited addExercise and addWorkout class

--- app/src/main/java/com/gymhomie/workouts/addWorkout.java
+++ app/src/main/java/com/gymhomie/workouts/addWorkout.java
@@ -2,6 +2,143 @@ package com.gymhomie.workouts;
 
 //adds workout to database
 
-public class addWorkout {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.gymhomie.R;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.gymhomie.Water_Intake_Activity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class addWorkout extends AppCompatActivity {
+
+    private EditText workoutName;
+    private ToggleButton arms, legs, glutes, chest, back, cardio, other;
+    private Button addExerciseButton, finish;
+    private FirebaseFirestore firebaseFirestore;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add_workout);
+
+        workoutName = findViewById(R.id.workout_name);
+
+        arms = findViewById(R.id.arms_toggle);
+        legs = findViewById(R.id.legs_toggle);
+        glutes = findViewById(R.id.glutes_toggle);
+        chest = findViewById(R.id.chest_toggle);
+        back = findViewById(R.id.back_toggle);
+        cardio = findViewById(R.id.cardio_toggle);
+        other = findViewById(R.id.other_toggle);
+
+        addExerciseButton = findViewById(R.id.add_exercise_button);
+        finish = findViewById(R.id.finish_button);
+
+        ArrayList<String> muscleGroup = null;
+        arms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> muscleGroup;
+                //TODO: add to list of muscle group; do for each toggle button
+            }
+            public void onToggleCLick(View view)
+            {
+                if (arms.isChecked())
+                {
+                    muscleGroup.add("Arms");
+                }
+                else if(legs.isChecked())
+                {
+                    muscleGroup.add("Legs");
+                }
+                else if(glutes.isChecked())
+                {
+                    muscleGroup.add("Glutes");
+                }
+                else if(chest.isChecked())
+                {
+                    muscleGroup.add("Chest");
+                }
+                else if(back.isChecked())
+                {
+                    muscleGroup.add("Back");
+                }
+                else if(cardio.isChecked())
+                {
+                    muscleGroup.add("Cardio");
+                }
+                else
+                {
+                    muscleGroup.add("Other");
+                }
+            }
+        });
+
+        ArrayList<exercise> exercises = null;
+        addExerciseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                new addExercise();
+            }
+        });
+
+        workout myWorkout = new workout(workoutName, muscleGroup, exercises);
+
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote(view);
+            }
+        });
+
+    }
+
+    public void saveNote(View v) {
+
+        int amount = amountPicker.getValue();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String userID = auth.getCurrentUser().getUid();
+        String collectionPath = "users/"+userID+"Workouts";
+
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_YEAR, year);
+        note.put(KEY_MONTH, month);
+        note.put(KEY_DAY, day);
+        note.put(KEY_AMOUNT, amount);
+
+        db.collection(collectionPath).document().set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Water_Intake_Activity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Water_Intake_Activity.this, "Error saving note!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
 }
--- app/src/main/java/com/gymhomie/workouts/workout.java
+++ app/src/main/java/com/gymhomie/workouts/workout.java
@@ -1,14 +1,35 @@
 package com.gymhomie.workouts;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.NumberPicker;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.gymhomie.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class workout {
 
-    String name;
-    List<String> muscleGroups;
-    List<exercise> exercises;
+    EditText name = null;
+    ArrayList<String> muscleGroups;
+    ArrayList<exercise> exercises;
 
-    public workout(String name, List<String> muscleGroups, List<exercise> exercises)
+    public workout(EditText name, ArrayList<String> muscleGroups, ArrayList<exercise> exercises)
     {
         this.name = name;
         this.muscleGroups = muscleGroups;
@@ -16,7 +37,7 @@ public class workout {
     }
 
     //gets name of the workout
-    public String getName() {return name;}
+    public EditText getName() {return name;}
 
     //gets list of muscle groups in one workout
     public List<String> getMuscleGroups() {return muscleGroups;}
@@ -26,17 +47,16 @@ public class workout {
 
 
     //sets name of the workout
-    public void setName(String name) {this.name = name;}
+    public void setName(EditText name) {this.name = name;}
 
     //sets list of muscle groups to the workout
-    public void setMuscleGroups(List<String> muscleGroups) {this.muscleGroups = muscleGroups;}
+    public void setMuscleGroups(ArrayList<String> muscleGroups) {this.muscleGroups = muscleGroups;}
 
     //sets list of exercises to the workout
-    public void setExercise(List<exercise> exercises) {this.exercises = exercises;}
+    public void setExercise(ArrayList<exercise> exercises) {this.exercises = exercises;}
 
     //adds exercise to exercise list
-    public void addExercise(exercise exercise) {exercises.add(exercise);}
-
+    //public void addExercise(exercise exercise) {exercises.add(exercise);}
 
 }
 
