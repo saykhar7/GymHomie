cdb1172a31e038ebc14cd63d0661a43dc02028b2
Shekhar Basnet <basnetshekhar0@gmail.com>
2023-09-23 22:49:03 -0700

    bar code scanner layout fix

---
+++ app/src/main/java/com/gymhomie/gymMembership/GymMembership.java
@@ -0,0 +1,16 @@
+package com.gymhomie.gymMembership;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gymhomie.R;
+
+public class GymMembership extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gym_membership);
+    }
+}
