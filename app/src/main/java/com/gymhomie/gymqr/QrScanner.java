d8de486d2a0775ee208c8f3248eabc7ea53b8be2
Shekhar Basnet <basnetshekhar0@gmail.com>
2023-09-28 11:49:11 -0700

    QR/Barcode Scanning Working

---
+++ app/src/main/java/com/gymhomie/gymqr/QrScanner.java
@@ -0,0 +1,16 @@
+package com.gymhomie.gymqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gymhomie.R;
+
+public class QrScanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
