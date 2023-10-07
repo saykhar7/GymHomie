53409888b9fd85eb5122eee806c39d95a6e7bc61
Gustavo Pech <gustavo.pech@student.csulb.edu>
2023-10-07 02:08:34 -0700

    Working on display friends on the homies page

--- app/src/main/java/com/gymhomie/fragments/homie_fragment.java
+++ app/src/main/java/com/gymhomie/fragments/homie_fragment.java
@@ -8,9 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.gymhomie.R;
 
 public class homie_fragment extends Fragment {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    String userID = auth.getCurrentUser().getUid();
+    String collectionPath = "users/"+userID+"/homies"; //path for the water intakes on firestore
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
