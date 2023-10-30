4a055a656e5ccfbacfb8a9e5038b854b5bee2680
aldodagio <103005119+aldodagio@users.noreply.github.com>
2023-10-29 20:56:35 -0700

    modified active goal ui

--- app/src/main/java/com/gymhomie/ActiveGoals_Activity.java
+++ app/src/main/java/com/gymhomie/ActiveGoals_Activity.java
@@ -2,16 +2,12 @@ package com.gymhomie;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.gymhomie.tools.Goal;
 
-import java.util.Map;
+import com.gymhomie.tools.Goal;
 
 public class ActiveGoals_Activity extends AppCompatActivity {
     private Goal goal = new Goal();
@@ -20,6 +16,7 @@ public class ActiveGoals_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_goals);
+
         LinearLayout goalContainer = findViewById(R.id.goalContainer);
 
         // Retrieve the parameters sent from Goal_Activity
@@ -31,44 +28,43 @@ public class ActiveGoals_Activity extends AppCompatActivity {
         String target = "";
         String type = "";
         if(title.equalsIgnoreCase("steps")){
-            TextView textView = new TextView(this);
+            TextView textView;
             int inner_count = 0;
-            for (String key : extras.keySet()){
-                if(key.equalsIgnoreCase("title")){
+            for (String key : extras.keySet()) {
+                if (key.equalsIgnoreCase("title")) {
                     // TODO: nothing to do here
                 } else {
                     int count = Integer.parseInt(key);
-                    if(count == 0){
+                    if (count == 0) {
                         // TODO: first goal entry
                         start_date = getIntent().getStringExtra(String.valueOf(count));
                         inner_count++;
-                    }
-                    else if(count % 4 == 0){
+                    } else if (count % 4 == 0) {
                         // TODO: new goal entry
                         inner_count = 0;
                         start_date = getIntent().getStringExtra(String.valueOf(count));
                         inner_count++;
-                    }
-                    else{
+                    } else {
                         // TODO: goal entry
-                        if(inner_count == 1){
+                        if (inner_count == 1) {
                             end_date = getIntent().getStringExtra(String.valueOf(count));
-                        }
-                        else if(inner_count == 2){
+                        } else if (inner_count == 2) {
                             type = getIntent().getStringExtra(String.valueOf(count));
-                        }
-                        else {
+                        } else {
                             target = getIntent().getStringExtra(String.valueOf(count));
                             text = "Goal Type: " + type + ", Target Steps: " + target + ", Start Date: " + start_date + ", End Date: " + end_date;
+                            textView = new TextView(this);
+                            View itemLayout = getLayoutInflater().inflate(R.layout.activity_active_goals, null);
+                            textView = itemLayout.findViewById(R.id.textView);
                             textView.setText(text);
-                            goalContainer.addView(textView);
+                            goalContainer.addView(itemLayout);
                         }
                         inner_count++;
                     }
                 }
             }
         } else if (title.equalsIgnoreCase("hydration")) {
-            TextView textView = new TextView(this);
+            TextView textView;
             int inner_count = 0;
             for (String key : extras.keySet()){
                 if(key.equalsIgnoreCase("title")){
@@ -97,8 +93,10 @@ public class ActiveGoals_Activity extends AppCompatActivity {
                         else {
                             target = getIntent().getStringExtra(String.valueOf(count));
                             text = "Goal Type: " + type + ", Target Ounces: " + target + ", Start Date: " + start_date + ", End Date: " + end_date;
+                            View itemLayout = getLayoutInflater().inflate(R.layout.activity_active_goals, null);
+                            textView = itemLayout.findViewById(R.id.textView);
                             textView.setText(text);
-                            goalContainer.addView(textView);
+                            goalContainer.addView(itemLayout);
                         }
                         inner_count++;
                     }
@@ -132,15 +130,17 @@ public class ActiveGoals_Activity extends AppCompatActivity {
                             target = getIntent().getStringExtra(String.valueOf(count));
                             text = "Target Weight: " + target + ", Start Date: " + start_date + ", End Date: " + end_date;
                             textView = new TextView(this);
+                            View itemLayout = getLayoutInflater().inflate(R.layout.activity_active_goals, null);
+                            textView = itemLayout.findViewById(R.id.textView);
                             textView.setText(text);
-                            goalContainer.addView(textView);
+                            goalContainer.addView(itemLayout);
                         }
                         inner_count++;
                     }
                 }
             }
         } else if (title.equalsIgnoreCase("exercise")) {
-            TextView textView = new TextView(this);
+            TextView textView;
             int inner_count = 0;
             for (String key : extras.keySet()){
                 if(key.equalsIgnoreCase("title")){
@@ -169,8 +169,11 @@ public class ActiveGoals_Activity extends AppCompatActivity {
                         else {
                             target = getIntent().getStringExtra(String.valueOf(count));
                             text = "Exercise Type: " + type + ", Target: " + target + ", Start Date: " + start_date + ", End Date: " + end_date;
+                            textView = new TextView(this);
+                            View itemLayout = getLayoutInflater().inflate(R.layout.activity_active_goals, null);
+                            textView = itemLayout.findViewById(R.id.textView);
                             textView.setText(text);
-                            goalContainer.addView(textView);
+                            goalContainer.addView(itemLayout);
                         }
                         inner_count++;
                     }
@@ -212,8 +215,10 @@ public class ActiveGoals_Activity extends AppCompatActivity {
                                 text = "Goal Type: " + type + ", Target Days: " + target + ", Start Date: " + start_date + ", End Date: " + end_date;
                             }
                             textView = new TextView(this);
+                            View itemLayout = getLayoutInflater().inflate(R.layout.activity_active_goals, null);
+                            textView = itemLayout.findViewById(R.id.textView);
                             textView.setText(text);
-                            goalContainer.addView(textView);
+                            goalContainer.addView(itemLayout);
                         }
                         inner_count++;
                     }
