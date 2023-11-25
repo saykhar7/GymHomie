f32e065e1ca0b09f5dac3697b84b27ad090959b6
Shekhar Basnet <basnetshekhar0@gmail.com>
2023-11-25 06:43:02 -0800

    supplements post ui

---
+++ app/src/main/java/com/gymhomie/supplements/Supplements.java
@@ -0,0 +1,16 @@
+package com.gymhomie.supplements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gymhomie.R;
+
+public class Supplements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplements);
+    }
+}
