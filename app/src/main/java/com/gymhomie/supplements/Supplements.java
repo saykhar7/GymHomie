01c174d34e1f32b9d3a9cd780e93c2da39dccfef
Shekhar Basnet <basnetshekhar0@gmail.com>
2023-11-25 09:14:00 -0800

    supplements font end done

--- app/src/main/java/com/gymhomie/supplements/Supplements.java
+++ app/src/main/java/com/gymhomie/supplements/Supplements.java
@@ -2,6 +2,7 @@ package com.gymhomie.supplements;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -9,13 +10,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.gymhomie.R;
 
 public class Supplements extends AppCompatActivity {
@@ -26,6 +31,8 @@ public class Supplements extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth auth = FirebaseAuth.getInstance();
     String userID = auth.getCurrentUser().getUid();
+
+    SuppmentAdapter suppmentAdapter;
 
     FloatingActionButton addSupplementsBtn;
 
@@ -57,7 +64,37 @@ public class Supplements extends AppCompatActivity {
     private void setupRecyclerView() {
 
 
+
+
+
+
+        Query query =  FirebaseFirestore.getInstance().collection("Supplements").orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<SupplementPost> options = new FirestoreRecyclerOptions.Builder<SupplementPost>()
+                .setQuery(query, SupplementPost.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        suppmentAdapter = new SuppmentAdapter(options, this);
+        recyclerView.setAdapter(suppmentAdapter);
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        suppmentAdapter.startListening();
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        suppmentAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        suppmentAdapter.notifyDataSetChanged();
+    }
 }
